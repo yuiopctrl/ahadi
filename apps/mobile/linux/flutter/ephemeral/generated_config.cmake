7b5662d7bede99b40ff15de2dd8603edbd536a06
fredxzem <fredxzem@hotmail.com>
2026-08-15 16:52:32 +0300

    feat: add generated CMake files for Flutter plugin registration and configuration

---
+++ apps/mobile/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/fredxzem/development/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/fredxzem/Desktop/customers/2026/ahadi/apps/mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/fredxzem/development/flutter"
+  "PROJECT_DIR=/home/fredxzem/Desktop/customers/2026/ahadi/apps/mobile"
+  "DART_DEFINES=QVBJX0JBU0VfVVJMPWh0dHBzOi8vYXBpLnl1aW9wLndvcmsvYXBpL3Yx,RkxVVFRFUl9CVUlMRF9OQU1FPTEuMC4w,RkxVVFRFUl9CVUlMRF9OVU1CRVI9MQ==,RkxVVFRFUl9WRVJTSU9OPTMuNDcuMA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NGNmMjQxNjQyNg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NWY3NzYyNTY3Mw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMy4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/fredxzem/Desktop/customers/2026/ahadi/apps/mobile/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/fredxzem/Desktop/customers/2026/ahadi/apps/mobile/lib/main.dart"
+)
